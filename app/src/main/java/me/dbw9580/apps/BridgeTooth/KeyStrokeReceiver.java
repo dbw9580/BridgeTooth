bd3b6a3e31987c97bf89d2f1e2725957c3341a16
dbw9580 <dbw9580@live.com>
2019-01-16 17:02:53 +0800

    Update KeyStrokeReceiver to receive external requests.

--- app/src/main/java/me/dbw9580/apps/BridgeTooth/KeyStrokeReceiver.java
+++ app/src/main/java/me/dbw9580/apps/BridgeTooth/KeyStrokeReceiver.java
@@ -5,18 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class KeyStrokeReceiver extends BroadcastReceiver {
     static String TAG = "KeyStrokeReceiver";
+    public static final String ACTION_SEND_KEY = "BridgeTooth.SEND_KEY";
+    public static final String EXTRA_DATA = "BridgeTooth.DATA";
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Received broadcast " + intent);
-        Intent start = new Intent();
-        start.setPackage(context.getPackageName())
-            .setClass(context, MainActivity.class)
-            .setFlags(FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(start);
+        if (ACTION_SEND_KEY.equals(intent.getAction()) &&
+                intent.getStringExtra(EXTRA_DATA) != null) {
+            Log.d(TAG, "starting service");
+            Intent send = new Intent(context, BluetoothKeyboardService.class);
+            send.setAction(BluetoothKeyboardService.ACTION_SEND_STRING);
+            String toSend = intent.getStringExtra(EXTRA_DATA);
+            send.putExtra(BluetoothKeyboardService.EXTRA_SEND_STRING, toSend);
+            context.startForegroundService(send);
+        }
     }
 
 }
